00aeb8b8f71684aff5529f9cd55da0043e62e5a4
Jon <jonathanringenberg@gmail.com>
2022-03-21 12:48:17 -0500

    Add files via upload

---
+++ calculate.js
@@ -0,0 +1,29 @@
+let val = 0;
+
+function initialize()
+{
+  val = document.getElementById('value').value;
+  val = parseInt(val)
+  setUp();
+}
+
+function setUp()
+{
+  document.getElementById('main').innerHTML = "<h3>$" + val + "</h3><br><input class = 'amounts' type = 'button' value = '$2' onclick = 'calTwo()'><input type = 'button' class = 'amounts' value = '$4' onclick = 'calFour()'><input type = 'button' class = 'amounts' value = '$5' onclick = 'calFive()'>";
+}
+
+function calTwo()
+{
+   val = val - 2;
+   setUp();
+}
+function calFour()
+{
+     val = val - 4;
+     setUp();
+}
+function calFive()
+{
+    val = val - 5;
+    setUp();
+}

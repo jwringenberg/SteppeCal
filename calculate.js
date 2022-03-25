d3783178335397ac005c79168be6e7ff580a5e0a
Jon <jonathanringenberg@gmail.com>
2022-03-25 08:37:53 -0500

    Update calculate.js

--- calculate.js
+++ calculate.js
@@ -68,14 +68,14 @@ function printBill()
    let part1 = "<p id = 'addition'>Sodas: " + soda + "<br>"; 
    let part2 = "Beers: " + beer + "<br>"; 
    let part3 = "Wine/Mix/Selzer: " + mix + "</p><hr>"; 
-   let part4 = "<p id = 'addition'>: Total $" +  total; 
+   let part4 = "<p id = 'addition'>Total: $" +  total; 
    let part5 = ""; 
    let part6 =  "<br><br><input type = 'button' class = 'bill' value = 'Retourner' onclick = 'setUp()'>"; 
    
    if(val < 0)
     {
        let x = Math.abs(val); 
-       part5 = "<br>You Owe $" +  x + "</p>"; 
+       part5 = "<br><span id = 'owe'>You Owe $" +  x + "</span></p>"; 
     }
    else if (val == 0)
     {
@@ -83,7 +83,7 @@ function printBill()
      }
     else
     {
-      part5 = "<br>We Owe You $" + val + "</p>"; 
+      part5 = "<br><span id = 'owe'>We Owe You $" + val + "</span></p>"; 
     }
   
     document.getElementById('main').innerHTML = "<h3>L\'Addition</h3>" + part1 + part2 + part3 + part4 + part5 + part6;
